96b64fae44a50255ef5ff5b2c670b98e71e1685b
Clemens Tolboom <clemens@build2be.com>
2022-07-23 11:01:21 +0200

    Make server optional

--- main.py
+++ main.py
@@ -8,17 +8,27 @@ class MainApplication(tk.Frame):
     def __init__(self, parent, *args, **kwargs):
         tk.Frame.__init__(self, parent, *args, **kwargs)
         self.parent = parent
+        self.server_process = None
+
+    def start_server(self):
+        web_dir = 'web'
+        self.server_process = mp.Process(target=run_fastapi, args=(web_dir,))
+        self.server_process.start()
+
+    def stop_server(self):
+        if self.server_process is not None:
+            self.server_process.kill()
+            self.server_process = None
 
 
 def run_gui():
     root = tk.Tk()
-    MainApplication(root).pack(side="top", fill="both", expand=True)
+    app = MainApplication(root)
+    app.pack(side="top", fill="both", expand=True)
 
-    web_dir = 'web'
-    p = mp.Process(target=run_fastapi, args=(web_dir,))
-    p.start()
+    app.start_server()
     root.mainloop()
-    p.kill()
+    app.stop_server()
 
 
 if __name__ == "__main__":
